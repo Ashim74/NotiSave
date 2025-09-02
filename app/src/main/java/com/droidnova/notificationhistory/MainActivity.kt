c0c2124d784ea5ccc95745208fc8d1d04f5cc424
Ashim khan <116441770+Ashim74@users.noreply.github.com>
2025-09-02 14:12:31 +0530

    fix: avoid nav bar overlap

--- app/src/main/java/com/droidnova/notificationhistory/MainActivity.kt
+++ app/src/main/java/com/droidnova/notificationhistory/MainActivity.kt
@@ -4,13 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.droidnova.notificationhistory.presentation.navigation.AppNavGraph
 import com.droidnova.notificationhistory.presentation.ui.theme.AppTheme
 
@@ -20,7 +17,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AppTheme {
-               AppNavGraph()
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .navigationBarsPadding()
+                ) {
+                    AppNavGraph()
+                }
             }
         }
     }
